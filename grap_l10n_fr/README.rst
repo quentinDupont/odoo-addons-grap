01b8dc3085148608150eb183ac15e458f5de8ed7
Quentin Dupont <quentin.dupont@ntymail.com>
2018-01-24 17:01:29 +0100

    [ADD] new module for GRAP l10nn_fr

---
+++ grap_l10n_fr/README.rst
@@ -0,0 +1,9 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+==============
+GRAP l10n_fr 
+==============
+
+Module pour créer les bonnes taxes à la création du plan comptable d'une nouvelle activité.
---
+++ grap_l10n_fr/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017, Grap
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "GRAP Change for l10n_fr",
+    "summary": "Module summary",
+    "version": "8.0.1.0.0",
+    "category": "Uncategorized",
+    "author": "GRAP",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "account",
+    ],
+    "data": [
+        'views/grap_account_tax_template.xml',
+    ],
+}
